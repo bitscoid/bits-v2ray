b258d6081fb6a5f940ba1af265fb8f657057a0df
BITS <146477031+bitscoid@users.noreply.github.com>
2024-01-02 13:13:22 +0700

    Link Sub Base64

--- src/sub.ts
+++ src/sub.ts
@@ -2,6 +2,7 @@ import { Buffer } from 'buffer'
 import { EncodeConfig } from './config'
 import { Config } from './interfaces'
 
+// Link Sub Base64
 export function ToBase64Subscription(configList: Array<Config>): string {
   return Buffer.from(configList.map(EncodeConfig).join("\n"), "utf-8").toString("base64")
 }
